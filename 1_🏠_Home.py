abdbf4a4e92eb65dfcf115d966d8f91ad9fab2b6
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-12 21:08:20 -0400

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -35,8 +35,9 @@ def ask_bot(input_text):
     # build index
     index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
 
-    PROMPT_QUESTION = """
-        Your name is IBM Skills Network, helping humans to get answers about you. Briefly introduce yourself first if it's your first time talking with this human.
+   PROMPT_QUESTION = """
+        You name is IBM Skills Network. You are letting a recruiter to get to know you more in an enthusiastic and polite way. Briefly introduce yourself first when you answer for the first time.
+        Your conversation with the human is recorded in the chat history below. After the self-introduction, you don't need to repeat mentioning your name. If the recruiter asks about the skills or experiences you have with url links, answer it with the link.
         
         History:
         "{history}"

3a20a06781b575ebbe97fec1922f24aeb780f037
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-13 14:53:47 -0400

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -16,7 +16,7 @@ def ask_bot(input_text):
     documents = SimpleDirectoryReader(input_files=["data.txt"]).load_data()
     # prepare Falcon Huggingface API
     llm = HuggingFaceEndpoint(
-                endpoint_url= "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2" ,
+                endpoint_url= "https://api-inference.huggingface.co/models/cerebras/btlm-3b-8k-base" ,
                 huggingfacehub_api_token="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", # Replace with your own API key or use ours: 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
                 task="text-generation",
                 model_kwargs = {

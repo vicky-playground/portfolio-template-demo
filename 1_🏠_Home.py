07c2e5d4ded141f6070f11122e0037e483b360d1
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-13 20:23:47 -0400

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -66,17 +66,7 @@ conversation_history = []
 documents = SimpleDirectoryReader(input_files=["data.txt"]).load_data()
 
 def ask_bot(input_text):
-```
-    llm = HuggingFaceEndpoint(
-                    endpoint_url="https://api-inference.huggingface.co/models/gpt2",
-                    #endpoint_url= "https://api-inference.huggingface.co/models/thenlper/gte-large" ,
-                    huggingfacehub_api_token="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", # Replace with your own API key or use ours: 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
-                    task="text-generation",
-                    model_kwargs = {
-                        "max_new_tokens":120 # define the maximum number of tokens the model may produce in its answer. Int (0-250)       
-                    }
-                )
-```
+
     # LLMPredictor: to generate the text response (Completion)
     llm_predictor = LLMPredictor(
             llm=llm_hub

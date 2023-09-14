e2a3a591d54453030076dfe55020c8d6012ab688
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-13 20:21:00 -0400

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -8,7 +8,56 @@ from langchain.llms import HuggingFaceEndpoint
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain import HuggingFaceHub
 from llama_index.prompts.prompts import SimpleInputPrompt
+from langchain.llms import HuggingFaceHub
+from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models import Model
 
+# Check for GPU availability and set the appropriate device for computation.
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+# Global variables
+conversation_retrieval_chain = None
+chat_history = []
+llm_hub = None
+embeddings = None
+
+Watsonx_API = "Your WatsonX API"
+Project_id= "Your Project ID"
+
+# Function to initialize the language model and its embeddings
+def init_llm():
+    global llm_hub, embeddings
+    
+    params = {
+        GenParams.MAX_NEW_TOKENS: 250, # The maximum number of tokens that the model can generate in a single run.
+        GenParams.MIN_NEW_TOKENS: 1,   # The minimum number of tokens that the model should generate in a single run.
+        GenParams.DECODING_METHOD: DecodingMethods.SAMPLE, # The method used by the model for decoding/generating new tokens. In this case, it uses the sampling method.
+        GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
+        GenParams.TOP_K: 50,          # The top K parameter restricts the token generation to the K most likely tokens at each step, which can help to focus the generation and avoid irrelevant tokens.
+        GenParams.TOP_P: 1            # The top P parameter, also known as nucleus sampling, restricts the token generation to a subset of tokens that have a cumulative probability of at most P, helping to balance between diversity and quality of the generated text.
+    }
+    
+    credentials = {
+        'url': "https://us-south.ml.cloud.ibm.com",
+        'apikey' : Watsonx_API
+    }
+    
+    LLAMA2_model = Model(
+        model_id= 'meta-llama/llama-2-70b-chat',
+        credentials=credentials,
+        params=params,
+        project_id=Project_id)
+
+    llm_hub = WatsonxLLM(model=LLAMA2_model)
+
+    #Initialize embeddings using a pre-trained model to represent the text data.
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": DEVICE}
+    )
+
+init_llm()
 
 # Store the conversation history in a List
 conversation_history = []
@@ -17,7 +66,7 @@ conversation_history = []
 documents = SimpleDirectoryReader(input_files=["data.txt"]).load_data()
 
 def ask_bot(input_text):
-
+"""
     llm = HuggingFaceEndpoint(
                     endpoint_url="https://api-inference.huggingface.co/models/gpt2",
                     #endpoint_url= "https://api-inference.huggingface.co/models/thenlper/gte-large" ,
@@ -27,13 +76,16 @@ def ask_bot(input_text):
                         "max_new_tokens":120 # define the maximum number of tokens the model may produce in its answer. Int (0-250)       
                     }
                 )
+"""
     # LLMPredictor: to generate the text response (Completion)
     llm_predictor = LLMPredictor(
-            llm=llm
+            llm=llm_hub
     )
                                      
     # Hugging Face models can be supported by using LangchainEmbedding to convert text to embedding vector	
-    embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
+    embed_model = LangchainEmbedding(embeddings)
+    #embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
+    
     # ServiceContext: to encapsulate the resources used to create indexes and run queries    
     service_context = ServiceContext.from_defaults(
             llm_predictor=llm_predictor, 

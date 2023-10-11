f62653a09bfcd174ad0488407102a509fc07061c
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-11 11:54:46 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,9 +1,9 @@
 info = {
    "About":"Hey there, I'm Vicky! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Data Science to Blockchain, Cloud, and more. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
-   "Medium":"https://medium.com/the-power-of-ai/about",
+   "Medium":"https://medium.com/@vicky-note/about",
    "City":"Toronto, Canada",
-   "Photo":"""<a href=\"https://cognitiveclass.ai/\"><img src=\"https://sn-portals-cognitiveclass.s3.us-south.cloud-object-storage.appdomain.cloud/lmzotycs3p11f936gx1u5drchdrk" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>"""
+   "Photo":"""<a href=\"https://cognitiveclass.ai/\"><img src=\"https://github.com/ibm-developer-skills-network/streamlit-chatbot-template/blob/main/images/profile.png" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>"""
 }
 
 embed_rss= {

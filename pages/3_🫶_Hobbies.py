78e74c86b4806f4d26423081e3586053f92d8bdd
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-11 11:52:16 -0400

    Update 3_🫶_Hobbies.py

--- pages/3_🫶_Hobbies.py
+++ pages/3_🫶_Hobbies.py
@@ -30,6 +30,6 @@ with col3:
 # -----------------  footer  ----------------- #
 footer="""
 <div class="footer">
-<p>Made by <a href="https://cognitiveclass.ai/" target="_blank">Cognitive Class</a></p></div>
+<p>Made by <a href="https://www.linkedin.com/in/vicky-tck/" target="_blank">Vicky Kuo</a></p></div>
 """
 st.markdown(footer,unsafe_allow_html=True)
